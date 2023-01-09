521b6c36c29eaa928a9cfe79429fb9f7538274e1
MikolaGil <mikolagil@gmail.com>
2023-01-09 18:48:39 +0100

    excercises

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,33 @@
-console.log('test')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Todo = ()=>{
+    const items = ['Item 1', 'Second'];
+    return(
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    );
+}
+
+const Header =()=>{
+    return(<h1>My Todo List</h1>)
+}
+
+const SearchPanel=()=> {
+    return <input placeholder='Seacrh...'/>;
+}
+const App = ()=>{
+    
+    return (
+        <div>
+            <Header/>
+            <SearchPanel/>
+            <Todo/>
+        </div>
+    );
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
