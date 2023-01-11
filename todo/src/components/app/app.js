31409bf371c8d32e9aa5bf0a17fd5eaaa702c1cb
MikolaGil <mikolagil@gmail.com>
2023-01-11 19:00:02 +0100

    fonts

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -25,11 +25,4 @@ const App = () =>{
     );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
 export default App;
